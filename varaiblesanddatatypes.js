d9fb8af2819fdcdb369d955043287e1e5981071c
vinayydv3695 <vinayydv343@gmail.com>
2024-07-13 17:55:29 +0530

    day 1 starting with varaibles and conversions .

--- varaiblesanddatatypes.js
+++ varaiblesanddatatypes.js
@@ -31,3 +31,38 @@ console.log(typeof(myobj))
 
 let myarray = [1,2,3,4,5,]
 console.log(typeof(myarray))
+
+
+// declare a variable using let assign an initial value reassign a new value and log both value in consloe
+let number1 = 'bro';
+let number2 = 56;
+bro = 'world'
+
+console.log(number1)
+console.log(bro)
+
+//we can't reassign value it give error that son of a holy lady
+const name1 = 'sister'
+sister = 'universe'
+console.log(name1)
+console.log(sister)
+
+
+let myNumber = 10;
+
+// Reassigning a new value
+myNumber = 20;
+
+// Logging both values
+console.log("Initial value:", myNumber); // Output: Initial value: 10
+console.log("Reassigned value:", myNumber); // Output: Reassigned value: 20
+
+
+const myConstant = 10;
+
+// Attempting to reassign a new value (will throw an error)
+// Uncommenting the following line will result in a TypeError: Assignment to constant variable
+// myConstant = 20;
+
+// Logging the initial value
+console.log("Initial value:", myConstant); // Output: Initial value: 10
