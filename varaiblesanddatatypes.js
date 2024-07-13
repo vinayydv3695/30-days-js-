d909249defbc4e7c7217d9c90135ba51955e1f3e
vinayydv3695 <vinayydv343@gmail.com>
2024-07-13 17:42:43 +0530

    day 1 starting with varaibles and conversions

--- varaiblesanddatatypes.js
+++ varaiblesanddatatypes.js
@@ -2,5 +2,32 @@
 
 var value = 45;
 console.log(value)
+ //declare a vraible using let  assign it a string value and log into the console
 let name = "zurasama"
 console.log("name")
+
+//const declaration
+const  over18 = true;
+console.log(typeof(over18))
+
+//diffrent data types
+let num = 45;
+console.log(typeof(num))
+
+let string = "zura";
+console.log(typeof("zura"))
+
+let  racist = false
+console.log(typeof(racist))
+
+const  Monstertrio = ["luffy","zoro","sanji"]
+let myobj = {
+
+    name: 'crocus',
+    age: 45
+      
+};
+console.log(typeof(myobj))
+
+let myarray = [1,2,3,4,5,]
+console.log(typeof(myarray))
